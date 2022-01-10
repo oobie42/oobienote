5eb5ab0cefd5727d3e19cf36bf9abddae46d4636
Bent Hagemark <benthagemark@bents-mbp.lan>
2022-01-09 16:25:51 -0800

    Use #each in App.svelte

--- svelte/App.svelte
+++ svelte/App.svelte
@@ -4,10 +4,10 @@ import Note from './Note.svelte';
 function semiToneSteps(steps) {
   return Math.pow(2, steps/12);
 }
+
+const chord = [0, 4, 7];
 </script>
 
-<Note initialFrequency={440}/>
-<br/>
-<Note initialFrequency={440 * semiToneSteps(4)}/>
-<br/>
-<Note initialFrequency={440 * semiToneSteps(7)}/>
+{#each chord as key}
+  <Note initialFrequency={440 * semiToneSteps(key)} />
+{/each}
